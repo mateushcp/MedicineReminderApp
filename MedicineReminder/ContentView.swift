638320e51debce7bd36bd8aff0039c9c91cdfa63
Mateus Coelho <teus.hcp@gmail.com>
2022-02-03 20:57:43 -0300

    Initial Commit

---
+++ MedicineReminder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MedicineReminder
+//
+//  Created by Mateus Henrique Coelho de Paulo on 03/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
