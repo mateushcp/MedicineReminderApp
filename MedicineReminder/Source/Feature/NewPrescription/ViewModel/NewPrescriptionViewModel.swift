56008850850f2294f53fc7bc513a8f128fd7aebd
Mateus Coelho <teus.hcp@gmail.com>
2022-08-31 20:37:40 -0300

    Removing comented code

--- MedicineReminder/Source/Feature/NewPrescription/ViewModel/NewPrescriptionViewModel.swift
+++ MedicineReminder/Source/Feature/NewPrescription/ViewModel/NewPrescriptionViewModel.swift
@@ -12,36 +12,38 @@ import UserNotifications
 class NewPrescriptionViewModel: NewPrescriptionViewModelProtocol {
     var prescriptions:[Prescription] = []
     var db:DBHelper = DBHelper()
+    private let hourRealTimeMultiplier: Double = 3600
+    private let minuteRealTimeMultiplier: Double = 60
     
     func storeData(name: String, timeToTime: String, firstTime: String) {
         db.insert(name: name, timeToTime: timeToTime, firstTime: firstTime)
     }
     
     private func handleTime(timeToTime: String) -> String {
-            var timeToTime = timeToTime
-            switch timeToTime {
-            case "2 em 2 horas":
-                timeToTime = "2"
-            case "4 em 4 horas":
-                timeToTime = "4"
-            case "6 em 6 horas":
-                timeToTime = "6"
-            case "8 em 8 horas":
-                timeToTime = "70"
-            case "12 em 12 horas":
-                timeToTime = "60"
-            default:
-                timeToTime = "8"
-            }
-            return timeToTime
+        var timeToTime = timeToTime
+        switch timeToTime {
+        case "2 em 2 horas":
+            timeToTime = "2"
+        case "4 em 4 horas":
+            timeToTime = "4"
+        case "6 em 6 horas":
+            timeToTime = "6"
+        case "8 em 8 horas":
+            timeToTime = "70"
+        case "12 em 12 horas":
+            timeToTime = "60"
+        default:
+            timeToTime = "8"
         }
+        return timeToTime
+    }
+
+    func handleToInt(time: String) -> Double {
+        let timeToInt = handleTime(timeToTime: time)
+        let myInt = (timeToInt as NSString).integerValue
         
-        func handleToInt(time: String) -> Double {
-            let timeToInt = handleTime(timeToTime: time)
-            let myInt = (timeToInt as NSString).integerValue
-            
-            return Double(myInt)
-        }
+        return Double(myInt)
+    }
     
     func returnHourFirst(time: String) -> Double {
         let hoursAndMinutes = time.components(separatedBy: ":")
@@ -61,19 +63,16 @@ class NewPrescriptionViewModel: NewPrescriptionViewModelProtocol {
         let minuteStripped = String(formatedString.dropFirst(2))
         let doubleHour = Double(hourStripped)!
         let doubleMinute = Double(minuteStripped)!
-        let hoursTime = (doubleHour * 3600) + ( doubleMinute * 60)
+        let hoursTime = (doubleHour * hourRealTimeMultiplier) + ( doubleMinute * minuteRealTimeMultiplier)
         return hoursTime
     }
     
     func handleTimeInterval(firstTime: String, timeToTime: String, checked: Bool) -> Double {
         var timeInterval: Double = 00.00
-        let hoursFromFirst = returnHourFirst(time: firstTime)
-        let minutesFromFirst = returnMinuteFirst(time: firstTime)
-        let hourDate = Calendar.current.component(.hour, from: Date())
         if checked {
-            timeInterval =  handleToInt(time: timeToTime) * 3600
+            timeInterval =  handleToInt(time: timeToTime) * hourRealTimeMultiplier
         } else {
-          //TODO: implementar regra para enviar caso selecione horario
+            //TODO: implementar regra para enviar caso selecione horario
         }
         
         return timeInterval
@@ -81,40 +80,15 @@ class NewPrescriptionViewModel: NewPrescriptionViewModelProtocol {
     
     func notifications(name: String, timeToTime: String, firstTime: String, checked: Bool) {
         let center = UNUserNotificationCenter.current()
-//        getTimeForNotification(string: firstTime)
         let content = UNMutableNotificationContent()
         content.title = "Chegou a hora de tomar \(name)"
         content.body = "Lembre que você deve ingerir este remédio de \(timeToTime)"
         content.sound = UNNotificationSound.default
-    
-//        let timeInterval = handleToInt(time: timeToTime) // * 3600
-      
-        
         let trigger = UNTimeIntervalNotificationTrigger(
             timeInterval: handleTimeInterval(firstTime: firstTime, timeToTime: timeToTime, checked: checked),
             repeats: true)
-//        db.createNotificationsTable()
-//        db.insertNotificationsTable(name: firstTime, timeToTime: getTimeForNotification(string: firstTime))
-        //primeiro alarme fixo vai ser definido aqui
-        //case horario == horarioatual: request = timeToTime(8em8) * 3600
-        //case horario < horarioatual: request = ((horarioAtual - firstTime) + TimeToTime(8em8h)) * 3600
-        // final = 8tempoemtempo - (19atual - 17primeiro)
-        // 8 - 2 = 6 horas
-        // finalDepois = 8
-        //case horario > horarioatual: request = ((horario - horarioAtual) + timeToTime) * 3600
-        
-//        DispatchQueue.global(qos: .background).async {
-////            self.db.readNotifications()
-//            //deletar notificaçao anterior
-//            //inserir nova notificaçao com base no banco para daqui 8 em 8, por ex
-//            let request = UNNotificationRequest(identifier: firstTime, content: content, trigger: trigger)
-//        }
-        
-        let requestFirstAlarm = ""
         let request = UNNotificationRequest(identifier: name, content: content, trigger: trigger)
         
         center.add(request)
     }
-
-    //atual + selecionado usar first time como identifiers
 }
